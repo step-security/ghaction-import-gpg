347e30a0b7b70ad162a3ffb175f7b3eb9e298c36
amanstep <aman@stepsecurity.io>
2025-09-11 21:26:50 +0530

    fix: fixed subscription check code

--- src/main.ts
+++ src/main.ts
@@ -4,36 +4,19 @@ import * as git from './git';
 import * as gpg from './gpg';
 import * as openpgp from './openpgp';
 import * as stateHelper from './state-helper';
+import axios, {isAxiosError} from 'axios';
 
 async function validateSubscription(): Promise<void> {
   const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
 
-  // Create a promise that rejects after a timeout
-  const timeoutPromise = new Promise<Response>((_, reject) => setTimeout(() => reject(new Error('Request timed out')), 3000));
-
   try {
-    // Race the fetch call against the timeout
-    const response = await Promise.race([fetch(API_URL, {method: 'GET'}), timeoutPromise]);
-
-    if (!(response instanceof Response)) {
-      throw new Error('Invalid response object');
-    }
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+    await axios.get(API_URL, {timeout: 3000});
   } catch (error) {
-    if (error instanceof Error) {
-      if (error.message === 'Network response was not ok') {
-        core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
-        process.exit(1);
-      } else if (error.message === 'Request timed out') {
-        core.info('Timeout or API not reachable. Continuing to next step.');
-      } else {
-        core.error('An unexpected error occurred: ' + error.message);
-      }
+    if (isAxiosError(error) && error.response?.status === 403) {
+      core.error('Subscription is not valid. Reach out to support@stepsecurity.io');
+      process.exit(1);
     } else {
-      core.error('An unknown error occurred.');
+      core.info('Timeout or API not reachable. Continuing to next step.');
     }
   }
 }
